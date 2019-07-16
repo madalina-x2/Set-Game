15d2dfe0c1230fb57365077a6ee6afbceff771dc
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-07-16 10:59:46 +0300

    added Card model

---
+++ Set-Game/Set-Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set-Game
+//
+//  Created by Madalina Sinca on 15/07/2019.
+//  Copyright © 2019 Madalina Sinca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
