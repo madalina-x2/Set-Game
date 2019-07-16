53f29b88bfe086f751b1ab332c1076a22ad9098f
Madalina Sinca <madalinasinca@Madalinas-MacBook-Pro.local>
2019-07-16 19:05:38 +0300

    added cards identifiers in view

--- Set-Game/Set-Game/ViewController.swift
+++ Set-Game/Set-Game/ViewController.swift
@@ -36,6 +36,8 @@ class ViewController: UIViewController {
         } else { print("chosen card was not in cardButtons") }
     }
     
+    @IBOutlet weak private var dealCardsButton: UIButton!
+    
     @IBAction private func dealCards(_ sender: UIButton) {
         game.dealThreeCards()
         updateViewFromModel()
@@ -45,5 +47,53 @@ class ViewController: UIViewController {
         // TODO
     }
     
+    private func attributedStringForCard(_ card: Card) -> NSAttributedString {
+        let cardSymbol: String = {
+            switch card.symbol {
+            case .triangle: return "▲"
+            case .circle: return "●"
+            case .square: return "■"
+            }
+        }()
+        
+        let color: UIColor = {
+            switch card.color {
+            case .red: return #colorLiteral(red: 0.968746841, green: 0.777038753, blue: 0.3501046896, alpha: 1)
+            case .green: return #colorLiteral(red: 0.8050321937, green: 0.3532711267, blue: 0.4588070512, alpha: 1)
+            case .purple: return #colorLiteral(red: 0.2275798917, green: 0.6626726985, blue: 0.6195083261, alpha: 1)
+            }
+        }()
+        
+        let strokeWidth: CGFloat = {
+            switch card.decoration {
+            case .striped: return 0
+            case .filled: return 5
+            case .outline: return 10
+            }
+        }()
+        
+        let attributes: [NSAttributedStringKey: Any] = [
+            .strokeColor: color,
+            .strokeWidth: strokeWidth,
+            .foregroundColor: color.withAlphaComponent({
+                switch card.decoration {
+                case .striped: return 0.35
+                case .filled: return 1.0
+                case .outline: return 0.60
+                }
+                }()
+            )
+        ]
+        
+        let cardTitle: String = {
+            switch card.number {
+            case .one: return cardSymbol
+            case .two: return cardSymbol + " " + cardSymbol
+            case .three: return cardSymbol + " " + cardSymbol + " " + cardSymbol
+            }
+        }()
+        
+        return NSAttributedString(string: cardTitle, attributes: attributes)
+    }
 }
 
